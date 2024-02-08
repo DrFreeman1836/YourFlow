848e5badda247938b038b43448f84480f269126c
Viktor <viktor4gafonov@yandex.ru>
2024-02-08 22:38:48 +0300

    state refactoring

--- src/main/java/main/state/StateService.java
+++ src/main/java/main/state/StateService.java
@@ -18,24 +18,44 @@ public class StateService {
    * Используется для сохранения состояния в командах
    */
   private final HashMap<User, String> lastMessageMap;
+  public void setLastMessageMap(String lastMessage, User user) {
+    if (lastMessageMap.containsKey(user)) {
+      lastMessageMap.remove(user);
+      lastMessageMap.put(user, lastMessage);
+    } else {
+      lastMessageMap.put(user, lastMessage);
+    }
+  }
+  public String getLastMessage(User user) {
+    return lastMessageMap.get(user);
+  }
+  public void clearLastMessage(User user) {
+    lastMessageMap.remove(user);
+  }
+
 
   /**
    * Сохранять сюда id сообщений первого входа(когда пользователя еще нет в БД)
    */
   private final HashMap<User, List<Long>> firstMessage;
+  public void addFirstMessage(User user, Long idMessage) {
+    if (firstMessage.containsKey(user)) {
+      firstMessage.get(user).add(idMessage);
+    } else {
+      firstMessage.put(user, Collections.singletonList(idMessage));
+    }
+  }
+  public List<Long> getFirstMessage(User user) {
+    return firstMessage.get(user);
+  }
+  public void clearFirstMessage(User user) {
+    firstMessage.remove(user);
+  }
 
   /**
    * Сохранять предыдущее меню
    */
   private final HashMap<User, String> level;
-
-  @Autowired
-  public StateService() {
-    this.level = new HashMap<>();
-    this.lastMessageMap = new HashMap<>();
-    this.firstMessage = new HashMap<>();
-  }
-
   public void setLastLevel(String lastLevel, User user) {
     if (level.containsKey(user)) {
       level.remove(user);
@@ -44,14 +64,19 @@ public class StateService {
       level.put(user, lastLevel);
     }
   }
+  public String getLastLevel(User user) {
+    return level.get(user);
+  }
 
-  public void setLastMessageMap(String lastMessage, User user) {
-    if (lastMessageMap.containsKey(user)) {
-      lastMessageMap.remove(user);
-      lastMessageMap.put(user, lastMessage);
-    } else {
-      lastMessageMap.put(user, lastMessage);
-    }
+  public void clearLastLevel(User user) {
+    level.remove(user);
+  }
+
+  @Autowired
+  public StateService() {
+    this.level = new HashMap<>();
+    this.lastMessageMap = new HashMap<>();
+    this.firstMessage = new HashMap<>();
   }
 
   /**
@@ -65,38 +90,6 @@ public class StateService {
       case StorageCommand.NAME -> Storage.class;
       default -> null;
     };
-  }
-
-  public String getLastMessage(User user) {
-    return lastMessageMap.get(user);
-  }
-
-  public String getLastLevel(User user) {
-    return level.get(user);
-  }
-
-  public void clearLastMessage(User user) {
-    lastMessageMap.remove(user);
-  }
-
-  public void clearLastLevel(User user) {
-    level.remove(user);
-  }
-
-  public void addFirstMessage(User user, Long idMessage) {
-    if (firstMessage.containsKey(user)) {
-      firstMessage.get(user).add(idMessage);
-    } else {
-      firstMessage.put(user, Collections.singletonList(idMessage));
-    }
-  }
-
-  public List<Long> getFirstMessage(User user) {
-    return firstMessage.get(user);
-  }
-
-  public void clearFirstMessage(User user) {
-    firstMessage.remove(user);
   }
 
 }
