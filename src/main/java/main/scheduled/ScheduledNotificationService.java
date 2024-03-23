616471ec9814084c0337b2f74a5cccd919b5eee8
Viktor <viktor4gafonov@yandex.ru>
2024-03-23 11:39:42 +0300

    add update message

--- src/main/java/main/scheduled/ScheduledNotificationService.java
+++ src/main/java/main/scheduled/ScheduledNotificationService.java
@@ -2,11 +2,17 @@ package main.scheduled;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import main.api.telegram.enums.ParseMode;
+import main.menu.InlineMenu;
+import main.menu.StorageMenu;
+import main.model.Task;
 import main.model.Users;
 import main.service.SendingMessageDecorator;
+import main.service.TaskService;
 import main.service.UserService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.User;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +20,29 @@ public class ScheduledNotificationService {
 
   private final UserService userService;
   private final SendingMessageDecorator sendingMessage;
+  private final TaskService taskService;
 
   @Scheduled(cron = "0 58 23 * * ?")
   public void loopDeleteMessage() {
-    List<Users> listSeller = userService.findAllUsers();
-    listSeller.forEach(sendingMessage::clearAllHistoryMessage);
+    List<Users> allUsers = userService.findAllUsers();
+    allUsers.forEach(sendingMessage::clearAllHistoryMessage);
+  }
+
+  @Scheduled(cron = "0 01 7 * * ?")
+  public void loopUpdateMessage() {
+    List<Users> allUsers = userService.findAllUsers();
+    allUsers.forEach(u -> {
+      User user = new User();
+      user.setId(u.getIdTelegram());
+      sendingMessage.sendInfoMessage(u, u.getChatId(), "Тут будут категории задач", ParseMode.NON , StorageMenu.getMainMenu());
+      List<Task> taskList = taskService.findTasksByUser(u);
+      sendingMessage.sendSimpleMessage(user, u.getChatId(), "Задачи:", StorageMenu.getMainMenu(), true);
+      taskList.forEach(t -> {
+        if (t.getIsActive())
+          sendingMessage.sendSimpleMessage(user, u.getChatId(), t.getCaption(), ParseMode.HTML,
+              InlineMenu.toTask(t.getId()), false);
+      });
+    });
   }
 
 }
